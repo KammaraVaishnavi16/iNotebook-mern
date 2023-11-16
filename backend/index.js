e4d7b306ee2417e9079857cde71fe0cee173dd2c
Vaishnavi <vaishnavi.kammara1@gmail.com>
2023-11-16 12:55:38 +0530

    edited about.js file

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,26 @@
 const connectToMongo = require("./db");
 const express = require("express");
 const path = require("path");
-const cors = require("cors"); // Add this line
-
+const cors = require("cors");
 connectToMongo();
 
 const app = express();
 const port = 5000;
 
-// Enable CORS for all routes
 app.use(cors());
 
-// Serve static files from the frontend
 // app.use(express.static("https://i-notebook-frontend-wheat.vercel.app"));
+
 app.use(express.static(path.join(__dirname, "../inotebook/build")));
+
+//to use request body we use middleware
 app.use(express.json());
-
-// Available routes
+//available routes
 app.get("/", (req, res) => {
-  res.send("Welcome to iNotebook");
+  res.send("Welecome to iNotebook");
 });
-
-// Route-specific CORS configuration
-// app.use("/api/auth", (req, res, next) => {
-//   // res.header(
-//   //   "Access-Control-Allow-Origin",
-//   //   "https://i-notebook-frontend-wheat.vercel.app"
-//   // );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
-
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
-
 app.listen(port, () => {
   console.log(`iNotebook backend app listening at http://localhost:${port}`);
 });
--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,7 +1,72 @@
 import React from "react";
 
 const About = () => {
-  return <div>This is about page</div>;
+  return (
+    <div>
+      <div className="container">
+        <h2>Welcome to iNotebook: Your Digital Note-Taking Haven</h2>
+        <p>
+          At iNotebook, we understand the power of ideas and the importance of
+          capturing them effortlessly. Our platform is designed to be your go-to
+          digital notebook, providing a seamless and intuitive way to organize
+          your thoughts, ideas, and important information.
+        </p>
+        <h3>Features:</h3>
+
+        <p>
+          <strong>User-Friendly Interface:</strong> Say goodbye to the hassle of
+          traditional note-taking. Our user-friendly interface allows you to
+          create and manage notes with ease.{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Customizable Note Entries:</strong> Tailor your notes to your
+          liking by giving each a unique name and adding a detailed description.
+          Whether it's a project outline, meeting minutes, or a personal diary
+          entry, make your notes truly yours.
+        </p>
+        <p>
+          {" "}
+          <strong>Cloud Storage:</strong> Never worry about losing your notes
+          again. iNotebook automatically saves your data to the cloud, ensuring
+          that your important information is securely stored and accessible from
+          anywhere, anytime.
+        </p>
+        <p>
+          <strong>Seamless Collaboration:</strong> Share your notes effortlessly
+          with colleagues, friends, or collaborators. Collaborative work has
+          never been this efficient.
+        </p>
+
+        <h3>Why Choose iNotebook?</h3>
+        <p>
+          <strong>Accessibility:</strong> Access your notes on any device with
+          an internet connection. Whether you're on your computer, tablet, or
+          smartphone, your notes are always within reach.
+        </p>
+        <p>
+          {" "}
+          <strong>Security:</strong> We prioritize the security of your data.
+          Our robust encryption measures keep your notes safe and secure.
+        </p>
+        <p>
+          <strong>Efficiency:</strong> Save time and streamline your workflow by
+          organizing your notes in one central location. No more digging through
+          paper notebooks or multiple apps.
+        </p>
+        <h3>Get Started Today:</h3>
+        <p>
+          Join the iNotebook community and take your note-taking experience to
+          the next level. Sign up now to start capturing, organizing, and
+          sharing your ideas effortlessly.
+        </p>
+        <strong>iNotebook - Where Ideas Take Flight.</strong>
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default About;
